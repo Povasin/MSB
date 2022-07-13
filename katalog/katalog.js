9bd612e6e36fc18a48a24b55d31254c466a833b7
ma.egorov <defoltmaks@gmail.com>
2022-07-13 21:06:22 +0300

    рабочая фильтрация

--- katalog/katalog.js
+++ katalog/katalog.js
@@ -12,13 +12,20 @@ const katalog__Checkbox = document.querySelectorAll(".katalog__Checkbox")
 const clear = document.getElementById("clear")
 const firstFilter = document.getElementById("firstFilter")
 let minGap = 0;
+
+window.addEventListener('mouseup', (e) => {
+    if (e.target.id === "slider-1" || e.target.id === "slider-2") {
+        renderKatalog(cubinsMass)
+    }
+})
+
 const cubinsMass = [
     {
         href: "",
         img: "/main/cubinsForWork.webp",
-        name: "бытовки прорабские",
+        name: "бытовки прорабские1",
         discount: "10000",
-        price:"9000₽",
+        price: 9000,
         star: "★★★★☆",
         size: "6х2,4х2,4м",
         content: 10
@@ -26,9 +33,9 @@ const cubinsMass = [
     {
         href: "",
         img: "/main/cubinsForWork.webp",
-        name: "бытовки прорабские",
+        name: "бытовки прорабские2",
         discount: "10000",
-        price:"9000₽",
+        price: 9000,
         star: "★★★★★",
         size: "6х2,4х2,4м",
         content: 10
@@ -36,9 +43,9 @@ const cubinsMass = [
     {
         href: "",
         img: "/main/cubinsForDress.webp",
-        name: "бытовка раздевалка",
+        name: "бытовка раздевалка3",
         discount: "9000",
-        price:"8000₽",
+        price: 8000,
         star: "★★★★☆",
         size: "6х2,4х2,50м",
         content: 8
@@ -46,9 +53,9 @@ const cubinsMass = [
     {
         href: "",
         img: "/main/cubinsForDress.webp",
-        name: "бытовка раздевалка",
+        name: "бытовка раздевалка4",
         discount: "9000",
-        price:"8000₽",
+        price: 8000,
         star: "★★★★☆",
         size: "6х2,4х2,50м",
         content: 10
@@ -56,9 +63,9 @@ const cubinsMass = [
     {
         href: "",
         img: "/main/cubinsForLive.webp",
-        name: "бытовка для проживания",
+        name: "бытовка для проживания5",
         discount: "9000",
-        price:"7500₽",
+        price: 7500,
         star: "★★★★★",
         size: "2,5х2,5х3,0м",
         content: 9
@@ -66,51 +73,72 @@ const cubinsMass = [
     {
         href: "",
         img: "/main/cubinsForLive.webp",
-        name: "бытовка для проживания",
+        name: "бытовка для проживания6",
         discount: "9000",
-        price:"7500₽",
+        price: 7500,
         star: "★★★☆☆",
         size: "2,5х2,5х3,0м",
         content: 8
     },
 ]
 
+const starsChecked = {
+    "★★★★★": false,
+    "★★★★☆": false,
+    "★★★☆☆": false,
+    "★★☆☆☆": false,
+    "★☆☆☆☆": false
+}
+katalogStar.addEventListener("click", (e)=>{
+    if (e.target.className == "katalog__Checkbox" ) {
+        starsChecked[e.target.value]=e.target.checked
+    }
+    renderKatalog(cubinsMass)
+})
+function allStarsFalse() {
+    let flag = true;
+    let massStars = Object.values(starsChecked)
+    massStars.forEach(x=>{
+        if (x) {
+            flag = false
+        }
+    })
+    return flag
+}
+
 function renderKatalog(mass) {
-    mass.forEach(item => {
+    const filterMass = filter(mass)
+    katalog__slider.innerHTML = ''
+    filterMass.forEach(item => {
         katalog__slider.insertAdjacentHTML("beforeend", ` <a href="${item.href}" class="card">
         <div class="fd-row">
             <p class="star">${item.star}</p>
             <p class="card__arrow">→</p>
         </div>
-        <img class="card__img" src="${location.origin+item.img}" alt="${item.name}">
+        <img class="card__img" src="${location.origin + item.img}" alt="${item.name}">
         <p class="rent">Аренда</p>
         <p class="info">${item.name}</p>          
         <div class="card__sale">
             <div class="fd-col">
                 <p class="discount">${item.discount}</p>
-                <p class="card__price">${item.price}</p> 
+                <p class="card__price">${item.price}₽</p> 
             </div>
             <button class="card__bag"><img src="../main/bag-_1_-1.webp" alt="корзина"></button>
         </div> 
     </a>`)
     });
-    filter(cubinsMass)
 }
+slideTwo();
 renderKatalog(cubinsMass)
 
+document.querySelector(".filter__open").addEventListener("click", () => { katalog__filter.style.right = 0 + "%" })
+document.querySelector(".filter__close").addEventListener("click", () => { katalog__filter.style.right = -50 + "%" })
 
-
-document.querySelector(".filter__open").addEventListener("click",()=>{ katalog__filter.style.right = 0 + "%"})
-document.querySelector(".filter__close").addEventListener("click", ()=>{ katalog__filter.style.right = -50 + "%"})
-window.onload = function(){
-    slideOne();
-    slideTwo();
-}
 function inputValue() {
-    if (parseInt(sliderTwo.value) >= katalog__price[0].value ) {
+    if (parseInt(sliderTwo.value) >= katalog__price[0].value) {
         sliderOne.value = katalog__price[0].value
         fillColor()
-    } else{
+    } else {
         sliderOne.value = katalog__price[0].value
         sliderTwo.value = katalog__price[1].value
         katalog__price[1].value = katalog__price[0].value
@@ -124,74 +152,79 @@ function inputValue() {
 }
 function noDigits(event) {
     if ("1234567890".indexOf(event.key) == -1)
-      event.preventDefault();
-  }
+        event.preventDefault();
+    renderKatalog(cubinsMass)
+}
 
-function slideOne(){
-    if(parseInt(sliderTwo.value) - parseInt(sliderOne.value) <= minGap){
+sliderOne.addEventListener("input", (e) => {
+    if (parseInt(sliderTwo.value) - parseInt(sliderOne.value) <= minGap) {
         sliderOne.value = parseInt(sliderTwo.value) - minGap;
     }
     katalog__price[0].value = sliderOne.value;
     fillColor();
-}
-function slideTwo(){
-    if(parseInt(sliderTwo.value) - parseInt(sliderOne.value) <= minGap){
+})
+
+function slideTwo() {
+    if (parseInt(sliderTwo.value) - parseInt(sliderOne.value) <= minGap) {
         sliderTwo.value = parseInt(sliderOne.value) + minGap;
     }
     katalog__price[1].value = sliderTwo.value;
     fillColor();
 }
-function fillColor(){
+function fillColor() {
     percent1 = (sliderOne.value / sliderMaxValue) * 100;
     percent2 = (sliderTwo.value / sliderMaxValue) * 100;
     sliderTrack.style.background = `linear-gradient(to right, #dadae5 ${percent1}% ,#5134c4 ${percent1}% ,#b856d4 ${percent2}%, #dadae5 ${percent2}%)`;
 }
 
-function filter(mass) {
-    return mass.filter(item=>{
-        katalog__price[0].value < item.price && katalog__price[1].value > item.price ? true : false         
-        katalog__price[2].value < item.content && katalog__price[3].value > item.content ? true : false    
-        katalogStar.addEventListener("click", (e)=>{
-            if (e.target.className == "katalog__Checkbox" ) {
-                e.target.value == item.star ? true : false         
-            }
-        })
-        katalogSize.addEventListener("click", (e)=>{
-            if (e.target.className == "katalog__Checkbox" ) {
-                e.target.value == item.size ? true : false
-            }
-        })
-        if (mass == []) {
-            katalog__slider.insertAdjacentHTML("beforeend", `<p>По вашему запросу ничего не найдено</p>`)
-        }
-        clear.addEventListener("click", ()=>{
-            katalog__price[0].value = 1000
-            katalog__price[1].value = 9000
-            sliderOne.value = katalog__price[0].value
-            sliderTwo.value = katalog__price[1].value
-            fillColor()
-            katalog__price[2].value = 1
-            katalog__price[3].value = 10
-            for (let i = 0; i < katalog__Checkbox.length; i++) {
-                katalog__Checkbox[i].checked = false
-            }
-        })
-    }) 
 
+
+function filter(mass) {
+    return mass.filter(item => {
+        let trueItem = true;
+        trueItem = katalog__price[0].value <= item.price && katalog__price[1].value >= item.price ? true : false
+        trueItem = katalog__price[2].value <= item.content && katalog__price[3].value >= item.content ? true : false
+
+        // сортировка по звёздам
+        trueItem = starsChecked[item.star] || allStarsFalse()
+
+        return trueItem
+        // katalogStar.addEventListener("click", (e)=>{
+        //     if (e.target.className == "katalog__Checkbox" ) {
+        //         e.target.value == item.star ? true : false         
+        //     }
+        // })
+        
+        // if (mass == []) {
+        //     katalog__slider.insertAdjacentHTML("beforeend", `<p>По вашему запросу ничего не найдено</p>`)
+        // }
+        // clear.addEventListener("click", ()=>{
+        //     katalog__price[0].value = 1000
+        //     katalog__price[1].value = 9000
+        //     sliderOne.value = katalog__price[0].value
+        //     sliderTwo.value = katalog__price[1].value
+        //     fillColor()
+        //     katalog__price[2].value = 1
+        //     katalog__price[3].value = 10
+        //     for (let i = 0; i < katalog__Checkbox.length; i++) {
+        //         katalog__Checkbox[i].checked = false
+        //     }
+        // })
+    })
 }
 
-firstFilter.addEventListener("click", ()=>{
-   if (firstFilter.value == "cheap") {
-    cubinsMass.sort(function(a, b) {
-        return a.price - b.price;
-    });
-   }
-   if (firstFilter.value == "expencive") {
-    cubinsMass.sort()
-   }
-   if (firstFilter.value == "popular") {
-    cubinsMass.sort(function(a, b) {
-        return a.name - b.name;
-    });
-   }
+firstFilter.addEventListener("click", () => {
+    if (firstFilter.value == "cheap") {
+        cubinsMass.sort(function (a, b) {
+            return a.price - b.price;
+        });
+    }
+    if (firstFilter.value == "expencive") {
+        cubinsMass.sort()
+    }
+    if (firstFilter.value == "popular") {
+        cubinsMass.sort(function (a, b) {
+            return a.name - b.name;
+        });
+    }
 })
